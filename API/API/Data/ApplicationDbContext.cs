3679cd7c452b4fbbeb052b6e8b8d162c6fcf02c9
João Fiens <aluno27214@ipt.pt>
2026-03-11 11:41:12 +0000

    criação da Base de Dados

--- API/API/Data/ApplicationDbContext.cs
+++ API/API/Data/ApplicationDbContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        // Definir as tabelas da Base de Dados
+        public DbSet<Photography> Photos { get; set; }
+        public DbSet<MyUser> Clients { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
     }
 }
